8efa2a3791dc87a520c38b1cc5b2e4385edac699
paulm <paulm@freegeek.org>
2011-08-10 13:13:23 -0700

    first commit

--- scribe_sched.sh
+++ scribe_sched.sh
@@ -8,3 +8,24 @@ EOF
         exit 0
 }
 
+  staff_members=(Amelia Darryl Jen Jessica Laurel Liane  Meredith Paul Richard Sean Tony) 
+
+// staff_members=(Amelia Darryl Jen Jessica Liane  Meredith Paul Richard Sean Tony)
+
+
+# associative array
+declare -A email_list
+email_list[Amelia]="alamb"
+email_list[Darryl]="darryl"
+email_list[Jen]="jhiggins"
+email_list[Jessica]="jbeckett"
+email_list[Laurel]="laurel"
+email_list[Liane]="liane"
+email_list[Meredith]="meredith"
+email_list[Paul]="paulm"
+email_list[Richard]="richard"
+email_list[Sean]="scellef"
+email_list[Tony]="tonyc"
+
+
+# echo test2 | mail -s test2 paulm@freegeek.org
