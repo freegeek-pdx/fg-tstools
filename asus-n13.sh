1558ac0b36fa16b51f468cee5d48c6b85c7944ae
paulm <paulm@freegeek.org>
2012-06-14 13:41:06 -0700

    initial commit

---
+++ asus-n13.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 'ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0b05", ATTR{idProduct}=="1784", RUN+="/sbin/modprobe -qba rt2870sta"'>>/etc/udev/rules.d/network_drivers.rules
+echo 'install rt2870sta /sbin/modprobe --ignore-install rt2870sta $CMDLINE_OPTS; /bin/echo "0b05 1784" > /sys/bus/usb/drivers/rt2870/new_id' >>/etc/modprobe.d/network_drivers.conf
