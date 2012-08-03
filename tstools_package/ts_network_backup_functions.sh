33fa01c54e6662e95c7e4d3c8770898ad9bfb8b7
paulm <paulm@freegeek.org>
2012-08-03 16:41:00 -0700

    debug

--- tstools_package/ts_network_backup_functions.sh
+++ tstools_package/ts_network_backup_functions.sh
@@ -176,6 +176,7 @@ restore_users(){
                 uid=$(echo $line | awk -F : '{print $3}')
                 gid=$(echo $line | awk -F : '{print $4}')
 		password=$(grep $user $path/shadow | awk -F: '{print $2}')
+echo "passwd $password :"
 		if ! user_restore=$(restore_user $path $user $uid $gid $password $extpath); then
 			echo "$user_restore"
 			return 3
