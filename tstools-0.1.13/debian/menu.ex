7f42aeacd6d9e8d3b8caafbdedab3dc79c541e4c
paulm <paulm@freegeek.org>
2013-05-31 17:19:21 -0700

    deleted *.ex files

--- tstools-0.1.13/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(tstools):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="tstools" command="/usr/bin/tstools"
--- tstools-0.1.13/debian/tstools.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the tstools package
-#
-0 4	* * *	root	[ -x /usr/bin/tstools_maintenance ] && /usr/bin/tstools_maintenance
